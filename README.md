e99724f50a09db49ab250494478f560f01808fed
Penulitmate-Panacea <32064190+Penulitmate-Panacea@users.noreply.github.com>
2017-12-09 22:13:10 -0500

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Geohashing
+An implementation of xkcd.com/426 in Python. 
+This acts as the minimal viable product. 
+Roadmap coming soon
--- scrape.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import urllib.request
-soup = BeautifulSoup(urllib.request.urlopen('https://www.marketwatch.com/investing/index/djia').read(),'html.parser')
-closingdiv = soup.find_all('td', class_ = 'table__cell u-semi')
-closing = closingdiv[0].text.replace(',', '')
-#closing = closing.text
-print(int(closing))
