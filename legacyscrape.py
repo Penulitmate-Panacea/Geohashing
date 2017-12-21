71fc95ec49e02ebd0fc439895faafacc5fb100cc
Penultimate-Panacea <32064190+Penultimate-Panacea@users.noreply.github.com>
2017-12-21 01:04:33 -0500

    Changed from the last closing to the first opening in order to comply with The Algorithim. Removed redundant main.py and core.py files. Renamed bs4.py to legacyscrape.py as to not conflict with package of the same name.

--- legacyscrape.py
+++ legacyscrape.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 #Legacy webscrape
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
 soup = BeautifulSoup(urlopen('http://www.marketwatch.com/investing/index/djia').read(), 'html.parser')
 yesterclosetab = soup.find_all('td', class__='table__cell u-semi')
 print("var:", yesterclosetab)  # debugging
--- oop.py
+++ oop.py
@@ -2,28 +2,43 @@
 """temporary function definition file, will eventually be different files for better separation of concerns"""
 
 
-def api_stock():
+def get_dji_api():
     """"Determine previous closing index of the Dow Jones Industrial Average"""
-    from requests import get
+    import requests
+    import datetime
     with open("key1.apikey") as key_file:
         keys = key_file.readlines()
-        api_username = keys[0].replace('\n', '')
+    api_username = keys[0].replace('\n', '')
     api_password = keys[1]
     base_url = "https://api.intrinio.com"
-    request_url = base_url + "/historical_data"
+    request_url = base_url + "/prices"
     query_params = {
 
-        'identifier': '$DJIA',
-        'item': 'level',
-        'page_size': '1'
+        'identifier': '$DJI',
+        'start_date': datetime.date.today().strftime("%Y-%m-%d"),
+        'end_date': datetime.date.today().strftime("%Y-%m-%d")
 
     }
-    response = get(request_url, params=query_params, auth=(api_username, api_password))
+    response = requests.get(request_url, params=query_params, auth=(api_username, api_password))
     if response.status_code != 200:
         print("API error code: ", response.status_code)
     data = response.json()['data']
-    last_day = data[0]
-    return last_day['value']
+    print(data)
+    previous_day = data[0]
+    return previous_day['close']
+
+
+def get_dji_scrape():
+    """Scrapes today's opening price from Google Finance"""
+    from bs4 import BeautifulSoup
+    from urllib.request import urlopen
+    from re import sub
+    google_finance = BeautifulSoup(urlopen('https://finance.google.com/finance?cid=983582').read(), 'html.parser')
+    snap_table = google_finance.find_all('table', attrs={"class": "snap-data"})
+    values = snap_table[0].find_all('td', attrs={"class": "val"})
+    opening_row = str(values[2])
+    opening_price = sub('[^\d|.]', '', opening_row)
+    return opening_price
 
 
 def get_location():
@@ -38,58 +53,63 @@ def get_location():
     return lat, lon, j
 
 
-def get_hash(closing):
+def create_hash(closing):
     """Creates the hash from the users location and closing price of the Dow Jones Industrial Index"""
     from datetime import date
     from hashlib import md5
-    digest = md5((date.today().strftime("%Y-%m-%d") + "-" + str(closing)).encode()).hexdigest()
+    pre_hash = date.today().strftime("%Y-%m-%d") + "-" + str(closing)
+    digest = md5((pre_hash).encode()).hexdigest()
     lat_add = int(digest[:16], 16) / 16 ** 16
     lon_add = int(digest[16:], 16) / 16 ** 16
     return lat_add, lon_add
 
 
-def get_goal(home, hash_add):
+def create_goal(home, hash_add):
     """Determines the goal for the geohash"""
-    lat = home[0] + hash_add[0]
-    lon = home[1] + hash_add[1]
+    lat_full = home[0] + hash_add[0]
+    lon_full = home[1] + hash_add[1]
+    lat = round(lat_full, 5)
+    lon = round(lon_full, 5)
     return lat, lon
 
 
-def export(lat, lon, location):
+def export_cli(lat, lon, location):
     """Shows the user the goal"""
     print("Your geohash for:", location['city'], ",", location['region_name'], ",", location['country_name'], "is:\n",
           str(lat), ",", str(lon))
 
 
-def export_map(home, result):
+def export_map_html(home, result):
     """Saves the starting location and resulting geohash in a html file"""
     from datetime import date
     from folium import Map, Marker, Icon
     try:
-        result_map = Map(location=[home[0], home[1]],  tiles="Stamen Toner")
+        result_map = Map(location=[home[0], home[1]], tiles="Stamen Toner")
         Marker(location=[home[2]['latitude'], home[2]['longitude']], popup='Starting Location',
-               icon=Icon(color='green')) \
-            .add_to(result_map)
+               icon=Icon(color='green')).add_to(result_map)
         Marker(location=[result[0], result[1]], popup='Geohash Location', icon=Icon(color='red')).add_to(result_map)
         path = ".\Saved\geohash_" + str(date.today().strftime("%Y-%m-%d")) + ".html"
         result_map.save(path)
     except FileNotFoundError:
         from os import mkdir
-        from datetime import date
-        from folium import Map, Marker, Icon
         mkdir(".\Saved")
         result_map = Map(location=[home[0], home[1]], tiles="Stamen Toner")
         Marker(location=[home[2]['latitude'], home[2]['longitude']], popup='Starting Location',
-               icon=Icon(color='green')) \
-            .add_to(result_map)
+               icon=Icon(color='green')).add_to(result_map)
         Marker(location=[result[0], result[1]], popup='Geohash Location', icon=Icon(color='red')).add_to(result_map)
         path = ".\Saved\geohash_" + str(date.today().strftime("%Y-%m-%d")) + ".html"
         result_map.save(path)
 
 
-last_close = api_stock()
+opening = get_dji_scrape()
 start = get_location()
-hash_dec = get_hash(last_close)
-goal = get_goal(start, hash_dec)
-export(goal[0], goal[1], start[2])
-export_map(start, goal)
+hash_dec = create_hash(opening)
+goal = create_goal(start, hash_dec)
+export_cli(goal[0], goal[1], start[2])
+export_map_html(start, goal)
+
+# TODO 30W fix
+# TODO 0001-0930 fix
+# TODO allow for historical dates
+# TODO confusion matrix testing
+# TODO GUI
